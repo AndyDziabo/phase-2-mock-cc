e72f70a00260992e26b11085176e1531f210bf63
Andy Dziabo <jameszabo@yahoo.com>
2022-09-08 12:14:30 -0400

    all deliverables complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  const [newQuery, setNewQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(res => res.json())
+      .then(data => setListings(data))
+  }, []);
+
+  function handleDelete(deletedListing){
+    const updatedListings = listings.filter((listing) => listing.id !== deletedListing.id);
+    setListings(updatedListings);
+  }
+
+  function handleQuery(query){
+    setNewQuery(query);
+  }
+
+  const listingsToDisplay = listings.filter((listing) => {
+    if(newQuery === "") return true;
+    return listing.description.toLowerCase().includes(newQuery.toLowerCase());
+  });
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onQuery={handleQuery} />
+      <ListingsContainer listings={listingsToDisplay} onDelete={handleDelete} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({onQuery}) {
+  const [query, setQuery] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    onQuery(e.target.search.value)
+
   }
 
   return (
@@ -12,8 +15,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
